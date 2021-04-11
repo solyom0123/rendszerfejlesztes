5d1d012b947873333a527bac46d2fff7a6da4e56
Horváth Sándor <sanyi402@gmail.com>
2021-04-11 09:54:17 +0200

    Refs foodlist by restaurant

--- src/Controller/FoodList.php
+++ src/Controller/FoodList.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Restaurant;
 use App\Form\RestaurantType;
+use App\Repository\FoodRepository;
 use App\Repository\RestaurantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,85 +13,18 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/food_list")
+ * @Route("/foodlist")
  */
 class FoodList extends AbstractController
 {
     /**
-     * @Route("/", name="restaurant_index", methods={"GET"})
+     * @Route("/{id}", name="food_list_index", methods={"GET"})
      */
-    public function index(RestaurantRepository $restaurantRepository): Response
+    public function index(FoodRepository $foodRepository,$id,RestaurantRepository $restaurantRepository): Response
     {
-
-            return $this->render('food_list/index.html.twig', [
-                'restaurants' => $restaurantRepository->findAll(),
+            $restaurant=$restaurantRepository->find($id);
+            return $this->render('food/index.html.twig', [
+                'food' => $foodRepository->findByRestaurant($restaurant),
             ]);
      }
-
-    /**
-     * @Route("/new", name="food_list_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $restaurant = new Restaurant();
-        $form = $this->createForm(RestaurantType::class, $restaurant);
-        $form->handleRequest($request);
-        $restaurant->setOwner($this->getUser());
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($restaurant);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_main');
-        }
-
-        return $this->render('food_list/new.html.twig', [
-            'restaurant' => $restaurant,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="food_list_show", methods={"GET"})
-     */
-    public function show(Restaurant $restaurant): Response
-    {
-        return $this->render('food_list/show.html.twig', [
-            'restaurant' => $restaurant,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="food_list_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Restaurant $restaurant): Response
-    {
-        $form = $this->createForm(RestaurantType::class, $restaurant);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('app_main');
-        }
-
-        return $this->render('food_list/edit.html.twig', [
-            'restaurant' => $restaurant,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="food_list_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Restaurant $restaurant): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$restaurant->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($restaurant);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('dashboard_clear',["type"=>"company"]);
-    }
 }
