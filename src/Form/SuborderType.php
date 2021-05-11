f298a23d0e085e3d4b33336177c30544b4c78532
Daniel <dnl885@gmail.com>
2021-05-11 10:46:44 +0200

    Courier nem listázódik fix

--- src/Form/SuborderType.php
+++ src/Form/SuborderType.php
@@ -21,10 +21,13 @@ class SuborderType extends AbstractType
 {
     private $session;
     private $restaurantRepository;
-    public function __construct(SessionInterface $session,RestaurantRepository $restaurantRepository)
+    private CourierDataRepository $cdr;
+
+    public function __construct(SessionInterface $session,RestaurantRepository $restaurantRepository, CourierDataRepository $cdr)
     {
         $this->session= $session;
         $this->restaurantRepository = $restaurantRepository;
+        $this->cdr = $cdr;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -33,6 +36,11 @@ class SuborderType extends AbstractType
             ->add('courier',EntityType::class,array(
                 'class' => User::class,
                 'multiple' => false,
+                'choice_label'=>function(User $user){
+                    $courier = $this->cdr->findOneBy(['user'=>$user]);
+
+                    return $courier->getName()." (azonosító: ".$courier->getId()."; jármű: ".$courier->getVehicleType().")";
+                },
                 'query_builder' => function (UserRepository $repository) use($builder){
                     $qb = $repository->createQueryBuilder('u');
                     /* @var $restaurant Restaurant*/
@@ -46,43 +54,70 @@ class SuborderType extends AbstractType
                         \Doctrine\ORM\Query\Expr\Join::WITH,
                         'u = c.user'
                     );
-                    $qb->where($qb->expr()->upper('?1').' like \'%'.$qb->expr()->upper('c.location').'%\'' );
-                    switch ($day){
-                        case "0":{
-                            $qb->andWhere(' c.fromWorkingDateSunday >= ?2 and ?2 <= c.toWorkingDateSunday');
-                            break;
-                        }
+
+                    switch ($day+1){
                         case "1":{
-                            $qb->andWhere(' c.fromWorkingDateMonday >= ?2 and ?2 <= c.toWorkingDateMonday');
+                            $qb->andWhere('c.fromWorkingDateMonday <= ?1 and ?2 >= c.toWorkingDateMonday');
                             break;
                         }
                         case "2":{
-                            $qb->andWhere(' c.fromWorkingDateTuesday >= ?2 and ?2 <= c.toWorkingDateTuesday');
+                            $qb->andWhere('c.fromWorkingDateTuesday <= ?1 and ?2 >= c.toWorkingDateTuesday');
                             break;
                         }
                         case "3":{
-                            $qb->andWhere(' c.fromWorkingDateWednesday >= ?2 and ?2 <= c.toWorkingDateWednesday');
+                            $qb->andWhere('c.fromWorkingDateWednesday <= ?1 and ?2 >= c.toWorkingDateWednesday');
                             break;
                         }
                         case "4":{
-                            $qb->andWhere(' c.fromWorkingDateThursday >= ?2 and ?2 <= c.toWorkingDateThursday');
+                            $qb->andWhere('c.fromWorkingDateThursday <= ?1 and ?2 >= c.toWorkingDateThursday');
                             break;
                         }
                         case "5":{
-                            $qb->andWhere('  c.fromWorkingDateFriday >= ?2 and ?2 <= c.toWorkingDateFriday');
+                            $qb->andWhere('c.fromWorkingDateFriday <= ?1 and ?2 >= c.toWorkingDateFriday');
                             break;
                         }
                         case "6":{
-                            $qb->andWhere(' c.fromWorkingDateSaturday >= ?2 and ?2 <= c.toWorkingDateSaturday');
+                            $qb->andWhere('c.fromWorkingDateSaturday <= ?1 and ?2 >= c.toWorkingDateSaturday');
+                            break;
+                        }
+                        case "7":{
+                            $qb->andWhere(' c.fromWorkingDateSunday <= ?1 and ?2 >= c.toWorkingDateSunday');
                             break;
                         }
                     };
-                    // the function returns a QueryBuilder object
-                    $qb->setParameter('1', $restaurant->getAddress())
-                        ->setParameter('2', $date->format('H:i:s'))
+
+                    $splitAddress = explode(' ',$restaurant->getAddress());
+
+                    $filter = '';
+
+                    foreach ($splitAddress as $sa){
+                        $filter.='%'.$sa.'%'.' ';
+                    }
+
+                    $lastKey = 2;
+
+                    $filter = trim($filter);
+
+                    $filterSplit = explode(' ',$filter);
+
+                    $ors = '';
+
+                    foreach($filterSplit as $k=>$f){
+                        $ors.='LOWER(c.location) LIKE LOWER(?'.($lastKey+($k+1)).') OR ';
+                    }
+
+                    $ors = substr($ors,0,strlen($ors)-4);
+
+                    $qb->andWhere($ors);
+
+                    foreach($filterSplit as $k=>$f){
+                        $qb->setParameter(($lastKey+($k+1)),$f);
+                    }
+
+                        $qb->setParameter('1', $date->format('H:i:s'))
+                        ->setParameter('2','23:59:59')
                         ->orderBy('c.id', 'ASC');
-                   dump($date->format('H:i:s'));
-                    dd($qb->getQuery());
+
                     return $qb;
                 }))
             ->add('waitingTime')
@@ -94,8 +129,7 @@ class SuborderType extends AbstractType
                     'in_progress'=> OrderStatus::$IN_PROGRESS,
                     'done'=> OrderStatus::$DONE
                 ]
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
